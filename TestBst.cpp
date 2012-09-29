16c8441b2b8bff958937dc56cc2ed76da3771d2c
Cody Herring <ydoch@yahoo.com>
2012-09-29 04:09:38 -0500

    added comments in TestBst.cpp
    I wanted to just check to make sure I could exit GitHub, re-enter, make
    changes, and sync changes----- basically test use of app

--- TestBst.cpp
+++ TestBst.cpp
@@ -4,18 +4,19 @@
 using namespace std;
 
 int main() {
+
   BST testBST;
   int data;
-  cout << "inserting 36\n";
+  cout << "inserting 36\n"; // Insertion of 36
   testBST.Insert(36);
-  cout << "inserting 5\n";
+  cout << "inserting 5\n"; // Insertion of 5
   testBST.Insert(5);
-  cout << "inserting 20\n";
+  cout << "inserting 20\n"; // Insertion of 20
   testBST.Insert(20);
-  cout << "inserting 10\n";
+  cout << "inserting 10\n"; // Insertion of 10
   testBST.Insert(10);
-  cout << "inserting 7\n";
+  cout << "inserting 7\n"; // Insertion of 7
   testBST.Insert(7);
-  cout << "calling PrintIn\n";
+  cout << "calling PrintIn\n"; // Call PrintIn
   testBST.PrintIn(cout);
 }
