5dacb8445a2dd31915560bfafc6765dea67e5e36
Cody Herring <ydoch@yahoo.com>
2012-10-03 14:49:42 -0500

    RemoveHelp?? - Check me

--- Bst.cpp
+++ Bst.cpp
@@ -170,3 +170,110 @@ void BST::FindHelper(int e, BNode * current){
 BNode * BST::AtCursor()const {
   return cursor;
 }
+
+int BST::RemoveHelp(int e)
+{
+    BNode *back;
+    BNode *temp;
+    BNode *delParent;    // Parent of node to delete
+    BNode *delNode;      // Node to delete
+    
+    temp = root;
+    back = NULL;
+    
+    // Find the node to delete 
+    while((temp != NULL) && (e != temp->e))
+    {
+        back = temp;
+        if(Key < temp->e)
+            temp = temp->left;
+        else
+            temp = temp->right;
+    }
+    
+    if(temp == NULL) // Didn't find the one to delete 
+    {
+        cout << "Key not found. Nothing deleted.\n";
+        return false;
+    }
+    else
+    {
+        if(temp == root) // Deleting the root 
+        {
+            delNode = root;
+            delParent = NULL; 
+        }                
+        else
+        {
+            delNode = temp;
+            delParent = back;
+        }
+    }
+    
+    // Case 1: Deleting node with no children or one child 
+    if(delNode->right == NULL)
+    {
+        if(delParent == NULL)    // If deleting the root    
+        {
+            root = delNode->left;
+            delete delNode;
+            return true;
+        }
+        else
+        {
+            if(delParent->left == delNode)
+                delParent->left = delNode->left;
+            else
+                delParent->right = delNode->left;
+            delete delNode;
+            return true;
+        }
+    }
+    else // There is at least one child 
+    {
+        if(delNode->left == NULL)    // Only 1 child and it is on the right
+        {
+            if(delParent == NULL)    // If deleting the root    
+            {
+                root = delNode->right;
+                delete delNode;
+                return true;
+            }
+            else
+            {
+                if(delParent->left == delNode)
+                    delParent->left = delNode->right;
+                else
+                    delParent->right = delNode->right;
+                delete delNode;
+                return true;
+            }
+        }
+        else // Case 2: Deleting node with two children 
+        {
+            // Find the replacement value.  Locate the node  
+            // containing the largest value smaller than the 
+            // key of the node being deleted.                
+            temp = delNode->left;
+            back = delNode;
+            while(temp->right != NULL)
+            {
+                back = temp;
+                temp = temp->right;
+            }
+            // Copy the replacement values into the node to be deleted 
+            delNode->Key = temp->Key;
+            delNode->fValue = temp->fValue;
+            delNode->iValue = temp->iValue;
+            strcpy(delNode->cArray, temp->cArray);
+            
+            // Remove the replacement node from the tree 
+            if(back == delNode)
+                back->left = temp->left;
+            else
+                back->right = temp->left;
+            delete temp;
+            return true;
+        }
+    }
+}
