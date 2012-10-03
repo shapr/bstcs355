b695567fe7e5fef2a4d0ce78d38cdea822c6818e
shapr <shae@ScannedInAvian.com>
2012-10-03 12:48:51 -0500

    added PrintPre and PrintPost and added to TestBst

--- Bst.cpp
+++ Bst.cpp
@@ -96,11 +96,35 @@ void BST::PrintInHelp(ostream & oss, BNode * n){
 #ifdef DEBUG
   cout << "entered PrintInHelp, node holds " << n->data << " left is " << n->left << " right is " << n->right << endl;
 #endif
-  if (n->left != NULL) PrintInHelp(oss, n->left);
-  // any left legs have been printed
+  if (n->left != NULL) PrintInHelp(oss, n->left);   // any left legs have been printed
   oss << n->data << endl; // Print itself
-  if (n->right != NULL) PrintInHelp(oss, n->right);
-  // return oss;
+  if (n->right != NULL) PrintInHelp(oss, n->right); // print right legs
+}
+
+void BST::PrintPost(ostream& oss){
+  PrintPostHelp(oss, root);
+}
+
+void BST::PrintPostHelp(ostream & oss, BNode * n){
+#ifdef DEBUG
+  cout << "entered PrintInHelp, node holds " << n->data << " left is " << n->left << " right is " << n->right << endl;
+#endif
+  if (n->left != NULL) PrintPostHelp(oss, n->left); // recursively print left branches
+  if (n->right != NULL) PrintPostHelp(oss, n->right); // recursively print right branches
+  oss << n->data << endl; // Print itself
+}
+
+void BST::PrintPre(ostream& oss){
+  PrintPreHelp(oss, root);
+}
+
+void BST::PrintPreHelp(ostream & oss, BNode * n){
+#ifdef DEBUG
+  cout << "entered PrintPreHelp, node holds " << n->data << " left is " << n->left << " right is " << n->right << endl;
+#endif
+  oss << n->data << endl; // Print itself
+  if (n->left != NULL) PrintPreHelp(oss, n->left); // recursively print left branches
+  if (n->right != NULL) PrintPreHelp(oss, n->right); // recursively print right branches
 }
 
 bool BST::Empty()const {
--- TestBst.cpp
+++ TestBst.cpp
@@ -6,16 +6,25 @@ using namespace std;
 int main() {
   BST testBST;
   int data;
-  cout << "inserting 36\n";
-  testBST.Insert(36);
-  cout << "inserting 5\n";
-  testBST.Insert(5);
   cout << "inserting 20\n";
   testBST.Insert(20);
-  cout << "inserting 10\n";
-  testBST.Insert(10);
-  cout << "inserting 7\n";
-  testBST.Insert(7);
+  cout << "inserting 15\n";
+  testBST.Insert(15);
+  cout << "inserting 30\n";
+  testBST.Insert(30);
+  cout << "inserting 5\n";
+  testBST.Insert(5);
+  cout << "inserting 18\n";
+  testBST.Insert(18);
+  cout << "inserting 40\n";
+  testBST.Insert(40);
+  cout << "inserting 25\n";
+  testBST.Insert(25);
+
   cout << "calling PrintIn\n";
   testBST.PrintIn(cout);
+  cout << "calling PrintPost\n";
+  testBST.PrintPost(cout);
+  cout << "calling PrintPre\n";
+  testBST.PrintPre(cout);
 }
